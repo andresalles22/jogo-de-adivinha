2aca655a012ed702b3b9de07ba6ec7ae9fdaf8bd
André Alves Rutter Salles <1000575@PPC01INFLL00308.pucminas.net>
2019-02-25 20:35:23 -0300

    Terceira Versao

--- jogo.rb
+++ jogo.rb
@@ -1,32 +1,54 @@
-puts "Nome: "
+def compara
+  numero2 = 10
+  (1..3).each do |contador|
+    puts "\nJogo de Adivinhação"
+    puts "Chute #{contador}: "
+    numero = gets.chomp.to_i
+
+    if numero == numero2
+      system('clear')
+      puts "\nJogo de Adivinhação"
+      puts "Seu nome: #{nome}"
+      puts 'Seu chute foi correto!!'
+      break
+
+    elsif numero > numero2
+      system('clear')
+      puts "\nJogo de Adivinhação"
+      puts 'Seu numero eh maior, chute outro mais baixo!!'
+
+    elsif numero < numero2
+      system('clear')
+      puts "\nJogo de Adivinhação"
+      puts 'Seu numero eh menor, chute outro mais alto!!'
+
+    end
+  end
+end
+
+puts 'Nome: '
 nome = gets
 
-numero2=151
+compara
 
-for contador in 1..4 do
-puts "Chute #{contador}: "
-numero=gets.chomp.to_i
+def jogarNovamente
+  puts 'Quer continuar o jogo?(1- sim e 2 - nao)'
+  continuar = gets.chomp.to_i
+  if continuar == 1
+    system('clear')
+    compara
 
-if numero == numero2
-system("clear")
-puts "Jogo de Adivinhação"
-puts "Seu nome: #{nome}"
-puts "Seu chute foi correto!!"
-break
+  elsif continuar == 2
+    system('pause')
 
-elsif numero > numero2
-system("clear")
-puts "Jogo de Adivinhação"
-puts"Seu numero eh maior, chute outro mais baixo!!"
-
-elsif numero < numero2
-system("clear")
-puts "Jogo de Adivinhação"
-puts "Seu numero eh menor, chute outro mais alto!!"
-
+  elsif continuar != 1 && continuar != 2
+    puts 'Opcao invalida!'
+    jogarNovamente
+  end
+  while continuar == 1
+    jogarNovamente
+    compara
+  end
 end
 
-end
-
-
-
+jogarNovamente
